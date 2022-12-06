1db5b868a72c8bada9b0275ed872abf5f6133fd9
Lara-Vitoria <larinhavitoria12@gmail.com>
2022-12-05 21:58:38 -0300

    Atualização

---
+++ js/scroll.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal({ reset:true});
+sr.reveal('.sobre', {duration : 3000, origin: 'left'});
+sr.reveal('.experiencia', {duration : 3000,  origin: 'rigth'});
+sr.reveal('.projetos', {duration : 3000,  origin: 'left'});
